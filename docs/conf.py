ef49cd775ab99e48e613ba2b7bee74e949397ae9
yajiez <yajiez.me@gmail.com>
2019-12-30 23:40:15 +0000

    Bump the version and update the environment

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ import os
 import sys
 import inspect
 
-version = release = '0.1.12'
+version = release = '0.1.13'
 
 __location__ = os.path.join(os.getcwd(), os.path.dirname(
     inspect.getfile(inspect.currentframe())))
