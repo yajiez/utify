19772ae9b67295b3fb7bae880ab6256c02a5f54d
yajiez <yajiez.me@gmail.com>
2020-01-03 21:15:43 +0000

    Bump the version

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ import os
 import sys
 import inspect
 
-version = release = '0.2.3'
+version = release = '0.2.5'
 
 __location__ = os.path.join(os.getcwd(), os.path.dirname(
     inspect.getfile(inspect.currentframe())))
